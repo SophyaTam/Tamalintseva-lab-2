f0d03cb8b7297b3b5d190887d42b725de044802e
Sophya <sophyatamalintseva@yandex.ru>
2024-10-06 17:33:26 +0700

    Добавление new, delate и delate[]

--- ConsoleApplication144/lab-2.cpp
+++ ConsoleApplication144/lab-2.cpp
@@ -12,12 +12,32 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
-    char AvailableOptions[N - 1][M - 1], AllVid[N][M], LastVid[N][M];
-    int Djanre=0,Turn=0, AllAdvert[N], LastAdvert[N], Back=0;
-    Advert advert;  // Создаем объект класса Advert
-    Video video;    // Создаем объект класса Video
 
-    advert.ShowAdv(); // Показываем рекламу
-    video.ChooseVid(); // Вызываем метод для выбора видео
+    // Память для массива видео
+    char(*AvailableOptions)[M] = new char[N][M];
+    char(*AllVid)[M] = new char[N][M];
+    char(*LastVid)[M] = new char[N][M];
+    int* AllAdvert = new int[N];
+    int* LastAdvert = new int[N];
+
+    int Djanre = 0, Turn = 0, Back = 0;
+
+    // Динамическое выделение объектов классов Advert и Video
+    Advert* advert = new Advert();
+    Video* video = new Video();
+
+    advert->ShowAdv(); // Показываем рекламу
+    video->ChooseVid(); // Вызываем метод для выбора видео
+
+    // Освобождение динамически выделенной памяти
+    delete[] AvailableOptions; // Освобождаем память для массива AvailableOptions
+    delete[] AllVid;          // Освобождаем память для массива AllVid
+    delete[] LastVid;         // Освобождаем память для массива LastVid
+    delete[] AllAdvert;       // Освобождаем память для массива AllAdvert
+    delete[] LastAdvert;      // Освобождаем память для массива LastAdvert
+    delete advert;            // Освобождаем память для объекта Advert
+    delete video;             // Освобождаем память для объекта Video
+
+    return 0;
 }
 
